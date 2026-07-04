d1f4c3d05a34ddc98be327a5c002263aafb5414f
V Teja <msshamala38@gmail.com>
2026-07-04 10:18:40 +0530

    commit4

--- Frontend/index.js
+++ Frontend/index.js
@@ -8,7 +8,7 @@ const users = [
   {
     "name" :"John Doe",
     "gender" : "Male",
-    "image" : "../images/john.png"
+    "image" : "../images/john (1).png"
   },
 ]
 let curUserId = 0;
@@ -21,6 +21,32 @@ function toggle(){
   document.getElementById("img").src=users[curUserId].image;
   // name
   document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+  
 }
 
-console.log("Hello from JS");
+
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
